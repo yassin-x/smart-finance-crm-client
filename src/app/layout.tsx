4f37ec4e0c88e5e56f7c8c772e3222de721961ca
yassin-x <yassin.ibrahim.hamed@gmail.com>
2026-06-29 18:12:57 +0300

    c3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import ReactQueryProvider from "@/providers/ReactQueryProvider";
 import { Toaster } from "sonner";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from "next/script";
+import Image from "next/image";
 
 const readex = Readex_Pro({
   subsets: ["latin", "arabic"],
@@ -41,6 +43,30 @@ export default function RootLayout({
             closeButton={true}
           />
         </ReactQueryProvider>
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+
+    fbq('init', '2735961770130265');
+    fbq('track', 'PageView');
+  `}
+        </Script>
+        <noscript>
+          <Image
+            height="1"
+            alt=""
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2735961770130265&ev=PageView&noscript=1"
+          />
+        </noscript>
       </body>
     </html>
   );
