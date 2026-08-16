ad16b1b76bbf4b6eb6f2617f04d22e0e373cc55e
Yassin Ibrahim <119485168+yassin-x@users.noreply.github.com>
2026-08-16 14:41:18 +0300

    Update getPrograms.tsx

--- src/app/(admin)/dashboard/admin/programs/_actions/getPrograms.tsx
+++ src/app/(admin)/dashboard/admin/programs/_actions/getPrograms.tsx
@@ -9,7 +9,7 @@ export function usePrograms() {
       const res = await api.get("/template");
       return res.data;
     },
-    staleTime: 1000 * 60 * 5,
+    staleTime: 0,
     gcTime: 1000 * 60 * 10,
     refetchOnWindowFocus: false,
     retry: false,
