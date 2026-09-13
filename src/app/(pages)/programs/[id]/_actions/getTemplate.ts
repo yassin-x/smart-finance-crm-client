a6e0693d0b15ceda2e4e57b6613a03414b374ddd
yassin-x <yassin.ibrahim.hamed@gmail.com>
2026-09-13 17:46:17 +0300

    locale

--- src/app/(pages)/programs/[id]/_actions/getTemplate.ts
+++ src/app/(pages)/programs/[id]/_actions/getTemplate.ts
@@ -6,7 +6,11 @@ export function useProgramTemplate(slug: string) {
   return useQuery({
     queryKey: queryKeys.programs.detail(slug),
     queryFn: async () => {
-      const res = await api.get(`/template/${slug}`);
+      const res = await api.get(`/template`, {
+        params: {
+          slug,
+        },
+      });
       return res.data;
     },
     staleTime: 1000 * 60 * 5,
